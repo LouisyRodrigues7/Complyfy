7033ef64b58129b268ae581f1bee7ad85648bba6
jaciara batista <andreandrade1920@gmail.com>
2025-04-29 12:39:48 -0300

    fix - importacao class View

--- app/src/main/java/com/example/senacplanner/Pilares/ListaAtividades.kt
+++ app/src/main/java/com/example/senacplanner/Pilares/ListaAtividades.kt
@@ -1,6 +1,7 @@
 package com.example.senacplanner.Pilares
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
@@ -14,13 +15,13 @@ class ListaAtividades : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_pilar)
+        setContentView(R.layout.activity_lista_atividades)
 
         viewPager = findViewById(R.id.viewPager)
         btnPrev = findViewById(R.id.btnPrev)
         btnNext = findViewById(R.id.btnNext)
 
-        val adapter = ListaPilaresAdapter(this)
+        val adapter = ListaAtividadesAdapter(this)
         viewPager.adapter = adapter
 
         btnPrev.setOnClickListener {
